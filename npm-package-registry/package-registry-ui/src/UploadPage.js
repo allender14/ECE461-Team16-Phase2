3149af2ab391f982329182ccff2c897d58573916
Patrick Thomas McLin <pmclin@eceprog5.ecn.purdue.edu>
2023-11-27 15:04:09 -0500

    upload stylized

--- npm-package-registry/package-registry-ui/src/UploadPage.js
+++ npm-package-registry/package-registry-ui/src/UploadPage.js
@@ -1,9 +1,17 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
 
 const UploadPage = () => {
   const [files, setFiles] = useState([]);
   const [fileName, setFileName] = useState('');
+  const defaultTheme = createTheme();
 
   const handleFileDrop = (event) => {
     event.preventDefault();
@@ -42,43 +50,70 @@ const UploadPage = () => {
   }
 
   return (
-    <div>
-      <h2>Upload Package</h2>
-
-      {/* Display the list of files above the drag-and-drop area */}
-      {files.length > 0 && (
-        <ul>
-          {files.map((file, index) => (
-            <li key={index}>
-              {file.name}
-              <button onClick={() => removeFile(index)}>Remove</button>
-            </li>
-          ))}
-        </ul>
-      )}
-
-      <div
-        className="upload-box"
-        onDrop={handleFileDrop}
-        onDragOver={(event) => event.preventDefault()}
-        style={{ border: '2px dashed #cccccc', padding: '20px', textAlign: 'center' }}
+    <ThemeProvider theme={defaultTheme}>
+      <AppBar position="relative">
+        <Toolbar>
+          <Typography variant="h6" color="inherit" noWrap>
+            Upload Package
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Box
+        sx={{
+          bgcolor: 'background.paper',
+          pt: 4,
+          pb: 6,
+          px: 4
+        }}
       >
-        <p>Drag your zipped package file here</p>
-        <p>or</p>
-        <input
-          type="file"
-          accept=".zip"
-          onChange={handleFileChange}
-        />
-      </div>
+        {/* Add a button to go back to the homepage */}
+        <Link to="/">
+            <Button variant="contained">Go Back to Homepage</Button>
+        </Link>
+        <Container maxWidth="sm">
+          <Typography variant="h5" align="center" color="text.secondary" paragraph>
+            Drag and drop or select a zip file of the package you would like to upload
+            to the registry. You may upload one or multiple files at once.
+          </Typography>
+        </Container>
+        <Container maxWidth="xl">
+          {/* Display the list of files above the drag-and-drop area */}
+          {files.length > 0 && (
+            <ul>
+              {files.map((file, index) => (
+                <li key={index}>
+                  {file.name}
+                  <Button onClick={() => removeFile(index)}>Remove</Button>
+                </li>
+              ))}
+            </ul>
+          )}
 
-      <button onClick={handleFileUpload} disabled={files.length === 0}>Upload</button>
+          <div
+            className="upload-box"
+            onDrop={handleFileDrop}
+            onDragOver={(event) => event.preventDefault()}
+            style={{ border: '2px dashed #cccccc', padding: '20px', textAlign: 'center' }}
+          >
+            <Typography variant="body1" gutterBottom>
+              Drag your zipped package file here
+            </Typography>
+            <Typography variant="body1" gutterBottom>
+              or
+            </Typography>
+            <input
+              type="file"
+              accept=".zip"
+              onChange={handleFileChange}
+            />
+          </div>
 
-      {/* Add a button to go back to the homepage */}
-      <Link to="/">
-        <button>Go Back to Homepage</button>
-      </Link>
-    </div>
+          <Button onClick={handleFileUpload} disabled={files.length === 0} variant="contained">
+            Upload
+          </Button>
+        </Container>
+      </Box>
+    </ThemeProvider>
   );
 }
 
